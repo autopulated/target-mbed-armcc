d1b068ff0df767de146f7ed97701b342b25293f5
James Crosby <James.Crosby@arm.com>
2015-08-17 16:54:18 +0100

    remove unnecessary definitinos to make the command line shorter

--- CMake/Platform/mbedOS-ARMCC-C.cmake
+++ CMake/Platform/mbedOS-ARMCC-C.cmake
@@ -11,7 +11,7 @@ endif()
 set(CMAKE_C_CREATE_SHARED_LIBRARY "echo 'shared libraries not supported' && 1")
 set(CMAKE_C_CREATE_SHARED_MODULE  "echo 'shared modules not supported' && 1")
 set(CMAKE_C_CREATE_STATIC_LIBRARY "<CMAKE_AR> -cr<LINK_FLAGS> <TARGET> <OBJECTS>")
-set(CMAKE_C_COMPILE_OBJECT        "${ARMCC_ENV} <CMAKE_C_COMPILER> ${YOTTA_TARGET_DEFINITIONS} <DEFINES> --gnu -c <FLAGS> -o <OBJECT> <SOURCE>")
+set(CMAKE_C_COMPILE_OBJECT        "${ARMCC_ENV} <CMAKE_C_COMPILER> <DEFINES> --gnu -c <FLAGS> -o <OBJECT> <SOURCE>")
 set(CMAKE_C_LINK_EXECUTABLE       "<CMAKE_LINKER> -o <TARGET> <OBJECTS> <LINK_LIBRARIES> <CMAKE_C_LINK_FLAGS> <LINK_FLAGS>")
 # fun fact! armcc doesn't preprocess .S files on windows (that's uppercase .s)
 # like gcc does (and like it does itself on Linux). So, we have to squeeze in
@@ -20,9 +20,9 @@ set(CMAKE_C_LINK_EXECUTABLE       "<CMAKE_LINKER> -o <TARGET> <OBJECTS> <LINK_LI
 if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Windows")
     # to do && we need to put an explicit cmd.exe /C in here: This is how CMake
     # composes other composite commands for Ninja, so we should be safe
-    set(CMAKE_ASM_COMPILE_OBJECT      "cmd.exe /C <CMAKE_C_COMPILER> ${YOTTA_TARGET_DEFINITIONS} <DEFINES> --gnu -E <FLAGS> -o <OBJECT>.pp.s <SOURCE> && <CMAKE_C_COMPILER> <DEFINES> --gnu --c90 -c <FLAGS> -o <OBJECT> <OBJECT>.pp.s")
+    set(CMAKE_ASM_COMPILE_OBJECT      "cmd.exe /C <CMAKE_C_COMPILER> <DEFINES> --gnu -E <FLAGS> -o <OBJECT>.pp.s <SOURCE> && <CMAKE_C_COMPILER> <DEFINES> --gnu --c90 -c <FLAGS> -o <OBJECT> <OBJECT>.pp.s")
 else()
-    set(CMAKE_ASM_COMPILE_OBJECT      "${ARMCC_ENV} <CMAKE_C_COMPILER> ${YOTTA_TARGET_DEFINITIONS} <DEFINES> --gnu -c <FLAGS> -o <OBJECT> <SOURCE>")
+    set(CMAKE_ASM_COMPILE_OBJECT      "${ARMCC_ENV} <CMAKE_C_COMPILER> <DEFINES> --gnu -c <FLAGS> -o <OBJECT> <SOURCE>")
 endif()
 
 set(CMAKE_C_FLAGS_DEBUG_INIT          "-O0 -g")
